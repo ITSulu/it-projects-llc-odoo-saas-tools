39d633fb5d41041196296301e172442d1255e319
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-04-27 22:50:44 -0400

    [13.0][MIG] Upgraded the saas_portal_templates addon to the Odoo CE 13.0 version

--- saas_portal_templates/__manifest__.py
+++ saas_portal_templates/__manifest__.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'SaaS Portal - templates',
-    'version': '11.0.1.0.0',
+    'version': '13.0.1.0.0',
     'author': 'Ivan Yelizariev, Nicolas JEUDY',
     'license': 'LGPL-3',
     'category': 'SaaS',
     "support": "apps@it-projects.info",
     'website': 'https://it-projects.info',
-    'depends': ['saas_portal'],
+    'depends': [
+        'saas_portal',
+    ],
     'data': [
         'views/website.xml',
     ],
