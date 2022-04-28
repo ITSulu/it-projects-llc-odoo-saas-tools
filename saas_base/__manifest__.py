6193e0b337c756d3385d7ee6bc9aeeb4a534f24c
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-04-28 08:31:13 -0400

    [13.0][MIG] Upgraded the saas_base addon to the Odoo CE 13.0 version

--- saas_base/__manifest__.py
+++ saas_base/__manifest__.py
@@ -1,12 +1,14 @@
 {
     'name': 'SaaS Base',
-    'version': '12.0.1.0.1',
+    'version': '13.0.1.0.1',
     'author': 'Cesar Lage, Nicolas JEUDY',
     'license': 'LGPL-3',
     'category': 'SaaS',
     "support": "apps@it-projects.info",
     'website': 'https://it-projects.info',
-    'depends': [],
+    'depends': [
+        'base',
+    ],
     'data': [],
     'installable': True,
 }
