de46964859804e62bc1916c2fca31807a6d2bd5f
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-04-27 23:09:32 -0400

    [13.0][MIG] Upgraded the saas_portal_sale_online addon to the Odoo CE 13.0 version

--- saas_portal_sale_online/__manifest__.py
+++ saas_portal_sale_online/__manifest__.py
@@ -5,8 +5,13 @@
     "support": "apps@it-projects.info",
     'website': "https://twitter.com/nasyrov_ildar",
     'category': 'SaaS',
-    'version': '11.0.1.0.0',
-    'depends': ['website_sale', 'saas_portal', 'saas_portal_sale', 'website_sale_require_login'],
+    'version': '13.0.1.0.0',
+    'depends': [
+        'saas_portal',
+        'saas_portal_sale',
+        'website_sale',
+        'website_sale_require_login',
+    ],
     'data': [
         'views/templates.xml',
     ],
